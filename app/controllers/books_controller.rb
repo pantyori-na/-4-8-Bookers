3e5f7c74c488f4540ab37b3ae429b7cbd92f9ba2
中村　圭介 <kouzu.keinaka8@gmail.com>
2019-09-14 07:48:21 +0000

    [finish]4-8課題

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,57 @@
 class BooksController < ApplicationController
+
   def index
   	#全体を取得
   	@books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
-  	@blog = Book.new
+  	@book = Book.new
   end
 
   def create
+    book = Book.new(book_params)
+    book.save
+    if book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book)
+    else
+      @book = book
+      @books = Book.all
+      render action: :index
+    end
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book)
+    else
+      @book = book.find(params[:id])
+      render action: :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  # Strong Parametesの定義
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
  resources :books
- root :to => 'books#index'
+ root :to => 'books#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
