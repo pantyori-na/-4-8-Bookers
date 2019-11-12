b9a6afe80d8fc9b3a4bf1eb6234a0df23fabc5e6
中村　圭介 <kouzu.keinaka8@gmail.com>
2019-11-12 05:05:22 +0000

    test_deploy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -33,7 +33,6 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
     if book.update(book_params)
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(book)
